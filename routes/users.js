84f137b61bbc147f2e955059f4331ea0dd34199d
pablo <juan.pablo.salgado@correounivalle.edu.co>
2022-05-18 22:30:20 -0500

    prueba

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.put('/', function(req, res, next) {
+  res.json(req.body)
+  // res.send('respond with a resource');
 });
 
 module.exports = router;

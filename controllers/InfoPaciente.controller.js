f62ddda69002744a2620bc477cbf4948e1154d35
AndresGiron <andresfelipe042001@gmail.com>
2022-05-30 19:00:34 -0500

    Controller y router de pacientes actualizados para la creacion de datos

---
+++ controllers/InfoPaciente.controller.js
@@ -0,0 +1,68 @@
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+
+
+const putUpdatePaciente = async (req,res) => {
+    //res.send(req.body)
+    const {id_paciente, tipo_id, identificacion,nombre, apellido, direccion, ciudad, telefono, correo, edad, nacimiento} = req.body
+    const update = await prisma.paciente.update({
+        where: {
+            id_paciente: id_paciente
+        },
+        data: {
+            tipo_id: tipo_id,
+            identificacion: identificacion,
+            nombre: nombre,
+            apellido: apellido,
+            direccion: direccion,
+            ciudad: ciudad,
+            telefono: telefono,
+            correo: correo,
+            edad: parseInt(edad),
+            nacimiento: new Date(nacimiento).toISOString()
+        }
+    })
+
+    console.log(update)
+    return res.json(update)
+}
+
+const putCreatePaciente = async (req,res) => {
+    const {id_paciente, tipo_id, identificacion, nombre, apellido, direccion, ciudad, telefono, correo, edad, nacimiento } = req.body
+    const create = await prisma.paciente.create({
+        data: {
+            id_paciente: id_paciente,
+            tipo_id: tipo_id,
+            identificacion: identificacion,
+            nombre: nombre,
+            apellido: apellido,
+            direccion: direccion,
+            ciudad: ciudad,
+            telefono: telefono,
+            correo: correo,
+            edad: parseInt(edad),
+            nacimiento: new Date(nacimiento).toISOString()
+        }
+    })
+    console.log(create)
+    return res.json(create)
+}
+
+const getPaciente = async (req,res) => {
+    console.log('Llegaron estos datos')
+    console.log(req.body)
+    const {id_paciente} = req.body
+    const resultado = await prisma.paciente.findUnique({
+        where: {
+            id_paciente: id_paciente
+        }
+    })
+    console.log(resultado)
+    return res.json(resultado)
+}
+
+module.exports = {
+    putUpdatePaciente,
+    putCreatePaciente,
+    getPaciente,
+}
